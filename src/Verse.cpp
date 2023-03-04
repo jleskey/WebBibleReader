98b07919336c8da455febb662eb357183910886e
Joseph Leskey <josleskey@mail.mvnu.edu>
2023-03-04 16:08:12 -0500

    Tidy Verse files

--- src/Verse.cpp
+++ src/Verse.cpp
@@ -5,47 +5,59 @@
 #include <iostream>
 using namespace std;
 
-Verse::Verse() {  	// Default constructor
-    verseText = "Uninitialized Verse!";
-    verseRef = Ref();
-} 
+Verse::Verse()
+{ // Default constructor
+  verseRef = Ref();
+  verseText = "Uninitialized Verse!";
+}
 
 // Parse constructor - pass verse string from file
-Verse::Verse(const string s) {
-	// use Ref constructor to create verseRef
-	// store the remainder of the string in verseText
-    // TODO: Make verse text extraction less hard-coded.
-    verseRef = Ref(s);
-    verseText = s.length() == 0 ? "[End of the text]" : s.substr(s.find(' '), s.length());
+Verse::Verse(const string s)
+{
+  // use Ref constructor to create verseRef
+  // store the remainder of the string in verseText
+
+  // TODO: Make verse text extraction less hard-coded.
+  // This would probably require the Ref class to consume part of the string.
+
+  verseRef = Ref(s);
+  verseText = s.length() == 0 ? "[End of the text]" : s.substr(s.find(' '), s.length());
 }
 
-string Verse::getVerse() {
-	return verseText;
+string Verse::getVerse()
+{
+  return verseText;
 }
 
-Ref Verse::getRef() {
-    return verseRef;
+Ref Verse::getRef()
+{
+  return verseRef;
 }
 
 // display reference and verse quite simply
-void Verse::display() {
-    verseRef.displayMarker();
-    cout << " " << verseText;
- }
+void Verse::display()
+{
+  verseRef.displayMarker();
+  cout << " " << verseText;
+}
 
 // display reference and verse in a flowing format
- void Verse::displayFlowing(bool startsPassage) {
-    if (startsPassage || verseRef.getVerse() == 1) {
-        cout << endl;
-        verseRef.displayHeader();
-        cout << endl << endl;
-    }
-    display();
+void Verse::displayFlowing(bool startsPassage)
+{
+  if (startsPassage || verseRef.getVerse() == 1)
+  {
     cout << endl;
- }
+    verseRef.displayHeader();
+    cout << endl
+         << endl;
+  }
+  display();
+  cout << endl;
+}
 
- // display reference and verse inline
-void Verse::displayFullInline() {
-    verseRef.display();
-    cout << " " << verseText;
- }
+// display reference and verse inline
+void Verse::displayFullInline()
+{
+  verseRef.display();
+  cout << " " << verseText;
+}
--- src/Verse.h
+++ src/Verse.h
@@ -4,34 +4,32 @@
 // A Verse object represents a complete Bible verse, including its reference
 //    * Ref ref          - the reference (e.g., 1:1:1)
 //    * string verseText - the actual verse text (without reference)
-//
 
 #ifndef Verse_H
 #define Verse_H
-#include <string> 
+#include <string>
 #include <stdlib.h>
 #include "Ref.h"
 using namespace std;
 
-class Verse {
- private:
-   Ref verseRef;   // The reference for this verse.
-   string verseText;  	// actual verse text (without reference)
+class Verse
+{
+private:
+  Ref verseRef;     // The reference for this verse.
+  string verseText; // actual verse text (without reference)
 
- public:
-   Verse();   	// Default constructor
-   
-   Verse(const string s);
-		// pass entire line from file including reference and text
+public:
+  Verse(); // Default constructor
+  Verse(const string s); // Extract verse from line of Bible input file
 
-   string getVerse();
-   Ref getRef();
+  // Accessors
+  Ref getRef();
+  string getVerse();
 
-   // Display Verse on cout
-   void display(); // Display ref & verse [with line breaks in needed].
-        // the reference should be displayed by calling verseref.display().
-   void displayFlowing(bool startsPassage); // Display ref & verse, formatted for flowing passages.
-   void displayFullInline(); // Display full ref & verse inline
+  // Display functions
+  void display();                          // Display ref & verse [with line breaks in needed].
+  void displayFlowing(bool startsPassage); // Display ref & verse, formatted for flowing passages.
+  void displayFullInline();                // Display full ref & verse inline
 };
 
-#endif //Verse_H
+#endif // Verse_H
